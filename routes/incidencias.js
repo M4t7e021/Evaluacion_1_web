1838ab76002ff99ddf7a71e27e8ead5d55bd7c1d
FerSerpas006 <00087325@uca.edu.sv>
2026-09-16 22:23:41 -0600

    Activando la ruta GET /incidencias/:id

--- routes/incidencias.js
+++ routes/incidencias.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/incidenciasController');
+const controller = require('../controllers/incidenciasControllers');
 
-// Tus dos endpoints asignados
 router.get('/', controller.listarIncidencias);
 router.post('/', controller.registrarIncidencia);
-
-// Conexión con los endpoints que desarrollarán tus compañeros
-router.get('/estadisticas', controller.obtenerEstadisticas);
 router.get('/:id', controller.buscarIncidenciaPorId);
-router.put('/:id/estado', controller.cambiarEstado);
-router.delete('/:id', controller.eliminarIncidencia);
-router.get('/:id/clasificacion', controller.obtenerClasificacion);
+
+// (AÚN SIN IMPLEMENTAR, POR ESO LO DEJE COMENTADO)
+// router.get('/estadisticas', controller.obtenerEstadisticas);
+// router.put('/:id/estado', controller.cambiarEstado);
+// router.delete('/:id', controller.eliminarIncidencia);
+// router.get('/:id/clasificacion', controller.obtenerClasificacion);
 
 module.exports = router;
